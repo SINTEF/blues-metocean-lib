89da87d28e8a10c4744d87208733d915702aa6e3
Lasse Bjermeland <lasse.bjermeland@sintef.no>
2023-02-03 10:03:56 +0100

    Scatter

--- src/bluesmet/common/scatter.py
+++ src/bluesmet/common/scatter.py
@@ -1,92 +1,118 @@
 import json
-from collections import Counter
 from math import floor
-from typing import Dict, Sequence, Tuple
+from typing import Callable, Dict, Sequence, Tuple
 
 import numpy as np
 from numpy import arange, ndarray, number
 
 
 class Scatter:
-    def __init__(self, bin_size=1.0, scatter_dict: Dict = None) -> None:
+    """A class for storing and combining scatter data
+    """
+
+    class Bins(dict):
+        """Based on collections.Counter but with support for adding lists"""
+
+        def __add__(self, other):
+            if not isinstance(other, Scatter.Bins):
+                return NotImplemented
+            result = Scatter.Bins()
+            for elem, count in self.items():
+                ov = other[elem]
+                newcount = count + ov
+                result[elem] = newcount
+            for elem, count in other.items():
+                if elem not in self:
+                    result[elem] = count
+            return result
+
+    def __init__(self, bin_size=1.0) -> None:
+        """
+        Parameters
+        ----------
+        bin_size : float
+            The bin size to use default is 1.0
+        """
         self.bin_size = bin_size
-        bins = Counter()
-        if scatter_dict:
-            for (idx, sdict) in scatter_dict.items():
-                cbin = Counter()
-                for jdx, dict2 in sdict.items():
-                    cbin[int(jdx)] = Counter(dict2.copy())
-                bins[int(idx)] = cbin
-        self.bins = bins
+        self.bins = Scatter.Bins()
 
     def add(self, x: number, y: number, **kwargs) -> Dict:
+        """Add a point to the scatter. Optionally add additional data stored in the bin"""
         xbin = floor(x / self.bin_size)
         ybin = floor(y / self.bin_size)
-        ybins: Counter = self.bins.get(xbin, Counter())
-        cbin = ybins.get(ybin, Counter())
+        ybins = self.bins.get(xbin, Scatter.Bins())
+        cbin = ybins.get(ybin, Scatter.Bins())
         cbin["occurences"] = cbin.get("occurences", 0) + 1
         for key, value in kwargs.items():
-            existing = cbin.get(key, 0.0)
-            cbin[key] = existing + value
+            existing = cbin.get(key, [])
+            existing.append(value)
+            cbin[key] = existing
         ybins[ybin] = cbin
         self.bins[xbin] = ybins
         return cbin
 
-    def combine(self, other):
+    def combine(self, other: "Scatter"):
+        """Combine with given scatter"""
         if self.bin_size != other.bin_size:
             raise ValueError("Bin sizes differ")
         xbins1 = self.bins
-        xbins2: Counter = other.bins
+        xbins2 = other.bins
         for (idx, counter) in xbins2.items():
-            existing: Counter = xbins1.get(idx)
+            existing = xbins1.get(idx)
             if existing:
                 # Add the counts
-                for (bin, count) in existing.items():
-                    newcount = counter.get(bin)
+                for (ybin, count) in existing.items():
+                    newcount = counter.get(ybin)
                     if newcount:
-                        existing[bin] = count + newcount
+                        existing[ybin] = count + newcount
 
-                for (bin, count) in counter.items():
-                    if bin not in existing:
-                        existing[bin] = count
+                for (ybin, count) in counter.items():
+                    if ybin not in existing:
+                        existing[ybin] = count
             else:
                 # New bin
                 xbins1[idx] = counter
 
     def num_rows(self):
+        """Number of rows"""
         bins = [int(row) for row in self.bins.keys()]
         return max(bins) + 1
 
     def num_columns(self):
+        """Number of columns"""
         largest = -1
         for ybin in self.bins.values():
             bins = [int(row) for row in ybin.keys()]
             largest = max(largest, max(bins))
         return largest + 1
 
-    def upper_rows(self) -> Sequence[number]:
+    def upper_rows(self) -> ndarray:
+        """Upper row values"""
         nrows = self.num_rows() + 1
         return arange(self.bin_size, nrows * self.bin_size, self.bin_size)
 
-    def upper_columns(self) -> Sequence[number]:
+    def upper_columns(self) -> ndarray:
+        """Upper column values"""
         ncols = self.num_columns() + 1
         return arange(self.bin_size, ncols * self.bin_size, self.bin_size)
 
     def occurences(self) -> ndarray:
+        """Occurences of all the bins"""
         nrows = self.num_rows()
         ncols = self.num_columns()
         occurences = np.zeros((nrows, ncols), dtype=int)
         for (row, ybins) in self.bins.items():
-            for (column, bin) in ybins.items():
-                occurences[row][column] = int(bin["occurences"])
+            for (column, ybin) in ybins.items():
+                occurences[row][column] = int(ybin["occurences"])
         return occurences
 
     def mean_xy(self) -> Tuple[number, number]:
+        """Mean x and y"""
         xp = 0.0
         yp = 0.0
         for (row, ybins) in self.bins.items():
-            for (column, bin) in ybins.items():
-                occ = float(bin["occurences"])
+            for (column, ybin) in ybins.items():
+                occ = float(ybin["occurences"])
                 x = (row + 0.5) * self.bin_size
                 y = (column + 0.5) * self.bin_size
                 xp += occ * x
@@ -96,53 +122,47 @@ class Scatter:
         yp = yp / all_occurences
         return (xp, yp)
 
-    def mean_of(self, key: str) -> number:
-        vp = 0.0
-        nvalues = 0
-        for ybins in self.bins.values():
-            for bin in ybins.values():
-                occ = float(bin["occurences"])
-                value = bin[key]
-                if occ > 0:
-                    vp += value
-                    nvalues += occ
-        if nvalues > 0:
-            return vp / nvalues
-        return 0.0
-
-    def get(self, key) -> ndarray:
+    def get_values(self, key, reducer: Callable[[Sequence],float]=None) -> ndarray:
+        """Get the values for a given key, reduse the list of values using the reducer or the mean if no reducer is given"""
         nrows = self.num_rows()
         ncols = self.num_columns()
         values = np.zeros((nrows, ncols), dtype=float)
         for (row, ybins) in self.bins.items():
-            for (column, bin) in ybins.items():
-                occurence = int(bin["occurences"])
-                if occurence > 0:
-                    values[row][column] = float(bin.get(key, 0.0)) / occurence
+            for (column, ybin) in ybins.items():
+                kvalues = ybin.get(key)
+                if kvalues:
+                    if reducer:
+                        values[row][column] = reducer(kvalues)
+                    else:
+                        values[row][column] = sum(kvalues) / len(kvalues)
         return values
 
     def __str__(self):
         return json.dumps(
-            {"nrows": self.num_rows(), "ncols": self.num_columns(), "bins": self.bins},
-            indent=4,
+            {
+                "row": self.upper_rows().tolist(),
+                "column": self.upper_columns().tolist(),
+                "occurences": self.occurences().tolist(),
+            },
         )
 
 
 def main():
-    bin_size = 2.0
+    """Demonstrate the scatter"""
+    bin_size = 4.0
     scatter1 = Scatter(bin_size)
     scatter1.add(10.0, 0.1, wavedir=10.0)
-    scatter1.add(11.0, 0.2, wavedir=10.0)
-    scatter1.add(10.0, 1.0, wavedir=10.0)
-    scatter1.add(10.0, 2.0, wavedir=10.0)
-    scatter1.add(10.1, 10.0, wavedir=10.0)
+    scatter1.add(11.0, 0.2, wavedir=11.0)
+    scatter1.add(10.0, 1.0, wavedir=12.0)
+    scatter1.add(10.0, 2.0, wavedir=12.0)
+    scatter1.add(10.1, 10.0, wavedir=13.0)
 
-    scatter2 = Scatter(bin_size, scatter1.bins)
-    scatter2.add(10.0, 0.1, wavedir=10.0)
-    scatter2.add(12.0, 0.2, wavedir=10.0)
-    scatter2.add(11.0, 1.0, wavedir=10.0)
-    scatter2.add(11.0, 2.0, wavedir=10.0)
-    scatter2.add(10.1, 10.0, wavedir=10.0)
+    scatter2 = Scatter(bin_size)
+    scatter2.add(10.0, 0.1, wavedir=15.0)
+    scatter2.add(12.0, 0.2, wavedir=15.0)
+    scatter2.add(11.0, 1.0, wavedir=20.0)
+    scatter2.add(11.0, 2.0, wavedir=17.0)
+    scatter2.add(10.1, 10.0, wavedir=9.0)
 
     print("scatter1=")
     print(scatter1)
@@ -155,10 +175,12 @@ def main():
     print("hs=" + str(scatter1.upper_rows()))
     print("tp=" + str(scatter1.upper_columns()))
     print(scatter1.occurences())
-    print(scatter1.get("wavedir"))
+    print("Scatter of wavedir=")
+
+
+    print(scatter1.get_values("wavedir"))
 
     print(scatter1.mean_xy())
-    print(scatter1.mean_of("wavedir"))
 
 
 if __name__ == "__main__":
